fe01c36220f6bd65720349e70adbc9d4b132ff92
opeide <fredrikopeide@gmail.com>
2018-03-02 01:14:24 +0100

    generates full triplets

--- batch_generator.py
+++ batch_generator.py
@@ -1,5 +1,6 @@
 import glob
 import numpy as np
+import random
 
 class BatchGenerator():
 
@@ -31,6 +32,7 @@ class BatchGenerator():
             self._test[obj] = self.load_object_poses(obj_folder, training_indexes.copy(), invert_index=True)
             self._train[obj].update(self.load_object_poses(obj_folder, training_indexes.copy(), invert_index=False))
 
+        self._train_flat = [[obj, img, self._train[obj][img]] for obj in self._train.keys() for img in self._train[obj].keys()]
         print('Finished Loading Dataset!')
 
 
@@ -44,15 +46,15 @@ class BatchGenerator():
             for line in f.readlines():
                 if '#' in line:
                     num = self._extract_number(line)
-                    img = ''
+                    img_path = ''
                     if (include_index is None) or (index==num and not invert_index) or (index!=num and invert_index):
                         if include_index is not None and include_index:
                             index = include_index.pop(0)
-                        img = line.strip(' #\n')
+                        img_path = folder_path + line.strip(' #\n')
                 else:
-                    if img:
+                    if img_path:
                         pose = [float(i) for i in line.strip('\n').split()]
-                        poses[img] = np.array(pose)
+                        poses[img_path] = np.array(pose)
         return poses
 
     def _extract_number(self, string):
@@ -62,17 +64,48 @@ class BatchGenerator():
 
     #for batch in gen.triplet_batches()
     def triplet_batches(self, batch_size=1, num_batches=1):
-        train_flat = [[obj, img, self._train[obj][img]] for obj in self._train.keys() for img in self._train[obj].keys()]
+        train_flat = self._train_flat.copy()
         np.random.shuffle(train_flat)
         for _ in range(num_batches):
-
-            batch = [train_flat.pop(0) for _ in range(batch_size) if train_flat]
+            batch = []
+            for _ in range(batch_size):
+                if not train_flat:
+                    return batch #yield batch return?
+                anchor = train_flat.pop(0)
+                _, anchor_img, _ = anchor
+                puller_img = self._get_puller(anchor)
+                pusher_img = self._get_pusher(anchor)
+                triplet = [anchor_img, puller_img, pusher_img]
+                batch.append(triplet)
             yield batch
 
 
 
     def _get_puller(self, anchor):
-        pass
+        #Find the most similar DB image of the same object type
+        anchor_obj, anchor_img, anchor_q = anchor
+        smallest_qam = 99   #know arbitrary bound
+        puller = ''
+        for img, q in self._db[anchor_obj].items():
+            qam = self._quaternion_angular_metric(anchor_q, q)
+            if qam < smallest_qam:
+                smallest_qam = qam
+                puller = img
+        return puller
+
+
+    def _get_pusher(self, anchor):
+    #random DB image of different object type than anchor
+        anchor_obj, anchor_img, _ = anchor
+        non_anchor_count = sum([len(self._db[obj]) for obj in self._db.keys() if obj != anchor_obj])
+        i = random.randint(0, non_anchor_count-1)
+        for obj in self._db.keys():
+            if obj == anchor_obj:
+                continue
+            if i - len(self._db[obj]) > 0:
+                i -= len(self._db[obj])
+                continue
+            return list(self._db[obj].keys())[i]
 
     def _quaternion_angular_metric(self,q1, q2):
         return 2*np.arccos(np.fabs(np.dot(q1, q2)))
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from batch_generator import BatchGenerator
+from PIL import Image
+import matplotlib.pyplot as plt
+
+#TODO: Normalize RGB channelse to zero mean and unit variance
+
 
 dataset_path = 'dataset'
 gen = BatchGenerator()
 gen.load_dataset(dataset_path)
 
-for batch in gen.triplet_batches(batch_size=1, num_batches=5):
+for batch in gen.triplet_batches(batch_size=5, num_batches=1):
     print(batch)
+    for triplet in batch:
+        fig = plt.figure()
+        for i in range(3):
+            fig.add_subplot(1, 3, i+1)
+            img = plt.imread(triplet[i])
+            plt.imshow(img)
+        plt.show()
