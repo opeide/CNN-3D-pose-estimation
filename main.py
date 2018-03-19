e66e58d7bd128d1d6647b910108f5253db1c313b
opeide <fredrikopeide@gmail.com>
2018-03-19 20:01:54 +0100

    Seemingly succssfully generates descriptors. able to train

--- main.py
+++ main.py
@@ -15,14 +15,16 @@ dataset_path = 'dataset'
 gen = BatchGenerator()
 gen.load_dataset(dataset_path)
 
-# i=0
-# for trip_batch in gen.train_input_fn(batch_size=1):
-#     i += 1
-#     if i > 1:
-#         break
-#     print(trip_batch[2,32,32])
 
+i=0
+for batch, labels in gen.train_input_gen(num_triplets=2):
+    i += 1
+    if i > 1:
+        break
+    print(np.shape(batch['x']))
 
 
 cnn = tf.estimator.Estimator(model_fn=model.cnn_model_fn)
-cnn.train(input_fn=lambda: next(gen.train_input_gen(batch_size=1)), steps=101)
+cnn.train(input_fn=lambda: next(gen.train_input_gen(num_triplets=5)), steps=200)
+
+#behaves as if channel first??
